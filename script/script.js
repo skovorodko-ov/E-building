390adbe53969bbe41bd479724e2a6b1511035aa5
Oleg Skovorodko <skovorodko29081989@gmail.com>
2021-09-28 22:29:31 +0300

    added menu

--- script/script.js
+++ script/script.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
-  const animateLogo = () => {
+  const firstLoading = () => {
     const logo = document.querySelector('.loading__logo'),
-          loadingBg = document.querySelector('.loading__bg');
+          loadingBg = document.querySelector('.loading__bg'),
+          section = document.getElementById('modal_loading'),
+          menu = document.querySelector('.desktop__menu');
 
     loadingBg.style.opacity = 1;
 
-    setInterval(() => {
+    const loadingSectionAnimat = setInterval(() => {
       if (loadingBg.getAttribute('style') === 'opacity: 1;') {
         loadingBg.style.opacity = 0.3;
         logo.style.transform = 'rotateY(360deg)';
@@ -17,8 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }, 2000);
 
-    
+    setTimeout(() => {
+      clearInterval(loadingSectionAnimat);
+      loadingBg.style.opacity = 1;
+      menu.style.opacity = 0.3;
+    }, 6000);
   };
 
-  animateLogo();
+  firstLoading();
 });
