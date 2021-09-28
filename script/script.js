dd1f605c75d02932326b3c3b7b7d255e27446afd
Oleg Skovorodko <skovorodko29081989@gmail.com>
2021-09-28 19:51:46 +0300

    no rotate logo

--- script/script.js
+++ script/script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(() => {
       if (loadingBg.getAttribute('style') === 'opacity: 1;') {
         loadingBg.style.opacity = 0.3;
-        logo.style.transform = 'rotateY(360deg)';
+        // logo.style.transform = 'rotateY(360deg)';
       } else {
         loadingBg.style.opacity = 1;
         logo.removeAttribute('style');
