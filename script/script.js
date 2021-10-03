e94dda1ad35168dbf96b47e32292c147c8802c3f
Oleg Skovorodko <skovorodko29081989@gmail.com>
2021-10-03 16:08:21 +0300

    added popups

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
+  const desktop = document.getElementById('modal_loading');
+
   const firstLoading = () => {
     const logo = document.querySelector('.loading__logo'),
           loadingBg = document.querySelector('.loading__bg'),
-          section = document.getElementById('modal_loading'),
           menu = document.querySelector('.desktop__menu');
 
     loadingBg.style.opacity = 1;
@@ -27,4 +28,82 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   firstLoading();
+
+  const popupCreating = (id) => {
+    const popup = document.createElement('section');
+
+    popup.classList.add('modal');
+    popup.classList.add('modal__active');
+    popup.setAttribute('id', id + 'modal');
+    popup.style.transform = `translate(${id * 10}px, ${id * 30}px)`;
+    popup.style.zIndex = 0;
+    popup.innerHTML = `
+      <div class="modal__header" id="${id}modalHeader">
+        <div class="modal__header__text">
+          <span class="modal__title">Название блока</span>
+        </div>
+        <button class="btn__close" id="${id}btn">
+          <img src="./img/closeIcon.svg" alt="close">
+        </button>
+      </div>
+      <div class="modal__container">
+
+      </div>
+    `;
+    desktop.append(popup);
+  };
+
+  const zeroZindexPopup = () => {
+    const popups = desktop.querySelectorAll('.modal'); 
+    popups.forEach(elem => {
+      elem.style.zIndex = 0;
+    });
+  };
+
+  const modalActivInDesktop = (id) => {
+    const activeModal = document.getElementById(id + 'modal');
+    activeModal.style.zIndex = 1;
+  };
+
+  const popupWindowOpen = () => {
+    const desktopMenu = document.querySelector('.desktop__menu');
+
+    desktopMenu.addEventListener('click', (event) => {
+      let target = event.target;
+
+      const activeModal = document.getElementById(target.getAttribute('id') + 'modal');
+
+      if (target.closest('.desktop__icon')) {
+        zeroZindexPopup();
+        if (!activeModal) {
+          popupCreating(target.getAttribute('id'));
+        } else {
+          activeModal.classList.add('modal__active');
+          modalActivInDesktop(target.getAttribute('id'));
+        }
+      }
+    });
+  };
+
+  const popupWindowClosing = () => {
+    desktop.addEventListener('click', (event) => {
+      let target = event.target;
+      let id = target.id ? target.id : target.parentNode.id;
+
+      if (target.closest('.btn__close')) {
+        const popups = desktop.querySelectorAll('.modal');
+        popups.forEach(elem => {
+          if (elem.id.slice(0, 1) === id.slice(0, 1)) {
+            elem.classList.remove('modal__active');
+          }
+        });
+      } else if (target.closest('.modal__active')){
+        zeroZindexPopup();
+        modalActivInDesktop(id.slice(0, 1));
+      }
+    });
+  };
+
+  popupWindowOpen();
+  popupWindowClosing();
 });
